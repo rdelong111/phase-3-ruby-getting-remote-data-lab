020e393d2a8042afae05d1a7a8fe5a15d40e1852
Ryan DeLong <rdelong111@gmail.com>
2022-08-17 08:42:41 -0500

    commit tree 08d00871f459b7390c2355e8003dce0cabdbe2de

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,15 @@
-# Write your code here
+require 'net/http'
+require 'open-uri'
+require 'json'
+
+class GetRequester(URL)
+  def get_response_body
+    uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end
+
+  def parse_json
+    JSON.parse(self.get_response_body)
+  end
+end
